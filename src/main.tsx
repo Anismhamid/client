b12b6579b9b8459a974469ee648c758ec0e364df
Anis Mhamid <anesmhamed1@gmail.com>
2025-08-29 00:57:42 +0300

    added vercel SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import {createRoot} from "react-dom/client";
+import {SpeedInsights} from "@vercel/speed-insights/react";
 import "./index.css";
 import App from "./App.tsx";
 import {BrowserRouter} from "react-router-dom";
@@ -11,6 +12,7 @@ window.Buffer = Buffer;
 createRoot(document.getElementById("root")!).render(
 	<GoogleOAuthProvider clientId={import.meta.env.VITE_API_GOOGLE_API}>
 		<UserProvider>
+			<SpeedInsights/>
 			<CartProvider>
 				<BrowserRouter>
 					<App />
