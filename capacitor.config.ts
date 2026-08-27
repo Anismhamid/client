9733342b4f075448b1bd270978d48db2c98789de
anis mhamed <anesmhamed1@gmail.com>
2026-08-27 15:02:28 +0300

    feat: update appId in capacitor.config.ts to reflect new application identifier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-    appId: 'com.safqa.app',
+    appId: 'com.safqa.c2c',
     appName: 'Safqa',
     webDir: 'dist',
 };
